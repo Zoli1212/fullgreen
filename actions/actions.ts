cac42f6d0459761b720d7294682d2952b5be2df0
butterfly-react <rdigital4455@gmail.com>
2024-12-11 00:36:20 +0100

    3:01

---
+++ actions/actions.ts
@@ -0,0 +1,20 @@
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
+import { parseWithZod} from '@conform-to/zod'
+;
+import { parse } from "path";
+import { productSchema } from "@/lib/zodSchemas";
+
+export async function createProduct(formData: FormData){
+
+    const { getUser } = getKindeServerSession()
+
+    const user = await getUser()
+
+    if(!user || user === null || !user.id || user.email !== "prosight11@gmail.com"){
+        return redirect('/')
+    }
+
+    const submission = parseWithZod(formData, { schema: productSchema })
+
+} 
--- app/dashboard/products/create/page.tsx
+++ app/dashboard/products/create/page.tsx
@@ -1,14 +1,99 @@
+'use client'
 import { Button } from "@/components/ui/button";
-import React from "react";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Select, SelectContent, SelectItem, SelectValue } from "@/components/ui/select";
+import { Textarea } from "@/components/ui/textarea";
+import { UploadDropzone } from "@/lib/uploadthing";
+import { SelectTrigger } from "@radix-ui/react-select";
+import { Switch } from "@radix-ui/react-switch";
+import { ChevronLeftIcon, Upload } from "lucide-react";
+import Link from "next/link";
+import React, { useState } from "react";
 
 type Props = {};
 
 function ProductCreateRoute({}: Props) {
+  const [images, setImages] = useState<string[]>([]);
   return (
     <form>
       <div className="flex items-center gap-4">
-        <Button></Button>
+        <Button variant='outline' size='icon' asChild>
+          <Link href='/dashboard/products'>
+       
+          <ChevronLeftIcon className="h-5 w-5" />
+          </Link>
+        </Button>
+        <h1 className='text-xl font-semibold tracking-tight'>New Product</h1>
       </div>
+      <Card className="mt-5">
+        <CardHeader>
+          <CardTitle>Product Details</CardTitle>
+          <CardDescription>In this form you can create your product</CardDescription>
+
+        </CardHeader>
+        <CardContent>
+          <div className='flex flex-col gap-6'>
+            <div className="flex flex-col gap-3">
+              <Label>Name</Label>
+              <Input type="text" className='w-full' placeholder='Product Name' />
+
+            </div>
+
+            <div className="flex flex-col gap-3">
+              <Label>Description</Label>
+              <Textarea placeholder='Write your description right here ...' />
+            </div>
+
+            <div className='flex flex-col gap-3'>
+              <Label>Price</Label>
+              <Input type='number' placeholder='$55' />
+
+            </div>
+            <div className='flex flex-col gap-3'>
+              <Label>Featured Product</Label>
+              <Switch />
+            </div>
+            <div className="flex flex-col gap-3">
+              <Label>Status</Label>
+              <Select>
+                <SelectTrigger>
+                  <SelectValue placeholder='Select Status' />
+
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value='draft'>Draft</SelectItem>
+                  <SelectItem value='published'>Published</SelectItem>
+                  <SelectItem value='archived'>Archived</SelectItem>
+
+                </SelectContent>
+
+              </Select>
+
+            </div>
+
+            <div className="flex flex-col gap-3">
+              <Label>Images</Label>
+              <UploadDropzone
+                  endpoint="imageUploader"
+                  onClientUploadComplete={(res) => {
+                    setImages(res.map((r) => r.url));
+                  }}
+                  onUploadError={() => {
+                    alert("Something went wrong");
+                  }}
+                />
+            </div>
+
+          </div>
+
+        </CardContent>
+        <CardFooter>
+          <Button>Create Product</Button>
+
+        </CardFooter>
+      </Card>
     </form>
   );
 }
